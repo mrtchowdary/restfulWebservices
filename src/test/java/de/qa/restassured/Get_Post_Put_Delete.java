870bff44aea85cc6b8251334d20b84834e05e8c2
ramum <mrtchowdary@gmail.com>
2020-06-29 11:26:57 +0530

    RestAssured practice using open weather API

--- src/test/java/de/qa/restassured/Get_Post_Put_Delete.java
+++ src/test/java/de/qa/restassured/Get_Post_Put_Delete.java
@@ -1,12 +1,19 @@
 package de.qa.restassured;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.json.JSONObject;
@@ -24,7 +31,7 @@ public class Get_Post_Put_Delete extends Base{
 	
 	String line = "=========================================================================";
 	
-	@Test(enabled=true)
+	@Test(enabled=false)
 	public void sampleDelete(){
 		RequestSpecification request = RestAssured.given();
 		request.header("Content-Type", "application/json");
@@ -136,4 +143,50 @@ public class Get_Post_Put_Delete extends Base{
 //		System.out.println("Status code is "+statusCode);
 //		Assert.assertEquals(statusCode, 200);
 //	}	
+	
+	@Test(enabled=false)
+	public void openWeatherCurrentWeatherData(){
+		RestAssured.baseURI = "http://api.openweathermap.org/data/2.5/weather";		//Current weather
+		RequestSpecification request = RestAssured.given();
+		request.contentType("application/json");
+		request.param("q","kakinada");
+		request.param("appid", "43b411d6f2697237d343d0d715b678ff");  //c498089bbd5181899d23e08e6b9486d2
+		
+		Response response = request.get("");
+		System.out.println("Response code is : "+response.getStatusCode());
+		System.out.println("Response Time is : "+response.getTime());
+		System.out.println("Status Line"+response.statusLine().toString());
+		System.out.println("Response body"+response.body().prettyPrint());
+		System.out.println("Response cookies"+response.cookies().toString());
+		
+		Headers headers = response.getHeaders();
+		for(Header header: headers){
+			System.out.println(header);
+		}
+	}
+	
+	//lat=17.69, lon=83.2093
+	@Test(enabled=true)
+	public void openWeatherFiveDayForecast(){
+		RestAssured.baseURI="http://api.openweathermap.org/data/2.5/forecast";
+		
+		RequestSpecification request = RestAssured.given();
+		request.contentType("application/json");
+		request.param("q", "visakhapatnam");
+		request.param("appid", "43b411d6f2697237d343d0d715b678ff");
+		
+		Response response = request.get();
+		
+		System.out.println("Status code : "+response.getStatusCode());
+		int surise = response.path("city.sunrise");
+		Date date = new Date(surise);
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss",Locale.getDefault());
+		System.out.println(df.format(date));
+		
+//		System.out.println("Users are \n");
+		String jsonString = response.asString();
+		Map<String, String> records = JsonPath.from(jsonString).get("city");
+		records.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(System.out::println);
+//		System.out.println(line);
+	}
 }
